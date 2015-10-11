13792a56a4cf512952e1becdbd9d5e13a5c70a2d
Shabir Mohamed <shabir_tck09@hotmail.com>
2015-10-11 16:09:58 +0530

    added new deviceType VirtualAgent

---
+++ modules/samples/virtual_firealarm/src/org.wso2.carbon.device.mgt.iot.sample.virtual.firealarm.plugin.impl/src/main/java/org/wso2/carbon/device/mgt/iot/sample/virtual/firealarm/plugin/impl/VirtualFireAlarmManagerService.java
@@ -0,0 +1,77 @@
+package org.wso2.carbon.device.mgt.iot.sample.virtual.firealarm.plugin.impl;
+
+import org.wso2.carbon.device.mgt.common.DeviceIdentifier;
+import org.wso2.carbon.device.mgt.common.DeviceManagementException;
+import org.wso2.carbon.device.mgt.common.DeviceManager;
+import org.wso2.carbon.device.mgt.common.app.mgt.Application;
+import org.wso2.carbon.device.mgt.common.app.mgt.ApplicationManagementException;
+import org.wso2.carbon.device.mgt.common.app.mgt.ApplicationManager;
+import org.wso2.carbon.device.mgt.common.operation.mgt.Operation;
+import org.wso2.carbon.device.mgt.common.spi.DeviceManagementService;
+import org.wso2.carbon.device.mgt.iot.sample.virtual.firealarm.plugin.constants
+		.VirtualFireAlarmConstants;
+
+import java.util.List;
+
+public class VirtualFireAlarmManagerService implements DeviceManagementService{
+	private DeviceManager deviceManager;
+	@Override
+	public String getType() {
+		return VirtualFireAlarmConstants.DEVICE_TYPE;
+	}
+
+
+	@Override
+	public String getProviderTenantDomain() {
+		return "carbon.super";
+	}
+
+	@Override
+	public boolean isSharedWithAllTenants() {
+		return true;
+	}
+
+	@Override
+	public String[] getSharedTenantsDomain() {
+		return new String[0];
+	}
+
+	@Override
+	public void init() throws DeviceManagementException {
+		this.deviceManager=new VirtualFireAlarmManager();
+	}
+
+	@Override
+	public DeviceManager getDeviceManager() {
+		return deviceManager;
+	}
+
+	@Override
+	public ApplicationManager getApplicationManager() {
+		return null;
+	}
+
+	@Override
+	public Application[] getApplications(String domain, int pageNumber, int size)
+			throws ApplicationManagementException {
+		return new Application[0];
+	}
+
+	@Override
+	public void updateApplicationStatus(DeviceIdentifier deviceId, Application application,
+										String status) throws ApplicationManagementException {
+
+	}
+
+	@Override
+	public String getApplicationStatus(DeviceIdentifier deviceId, Application application)
+			throws ApplicationManagementException {
+		return null;
+	}
+
+	@Override
+	public void installApplication(Operation operation, List<DeviceIdentifier> deviceIdentifiers)
+			throws ApplicationManagementException {
+
+	}
+}
---
+++ modules/samples/virtual_firealarm/units/public/js/virtual_firealarm.js
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+var modalPopup = ".wr-modalpopup";
+var modalPopupContainer = modalPopup + " .modalpopup-container";
+var modalPopupContent = modalPopup + " .modalpopup-content";
+var body = "body";
+
+/*
+ * set popup maximum height function.
+ */
+function setPopupMaxHeight() {
+    $(modalPopupContent).css('max-height', ($(body).height() - ($(body).height() / 100 * 30)));
+    $(modalPopupContainer).css('margin-top', (-($(modalPopupContainer).height() / 2)));
+}
+
+/*
+ * show popup function.
+ */
+function showPopup() {
+    $(modalPopup).show();
+    setPopupMaxHeight();
+}
+
+/*
+ * hide popup function.
+ */
+function hidePopup() {
+    $(modalPopupContent).html('');
+    $(modalPopup).hide();
+}
+
+/*
+ * DOM ready functions.
+ */
+$(document).ready(function () {
+    attachEvents();
+});
+
+function attachEvents() {
+    /**
+     * Following click function would execute
+     * when a user clicks on "Download" link
+     * on Device Management page in WSO2 DC Console.
+     */
+    $("a.download-link").click(function () {
+        var sketchType = $(this).data("sketchtype");
+        var deviceType = $(this).data("devicetype");
+        var downloadDeviceAPI = "/iotserver/api/device/sketch/generate_link";
+        var payload = {"sketchType":sketchType, "deviceType":deviceType};
+        console.log(payload);
+        $(modalPopupContent).html($('#download-device-modal-content').html());
+        showPopup();
+
+        $("a#download-device-download-link").click(function () {
+            invokerUtil.post(
+                downloadDeviceAPI,
+                payload,
+                function (data, textStatus, jqxhr) {
+                    if(jqxhr.status == 200) {
+                        if(data == "403"){
+                            $(modalPopupContent).html($('#device-403-content').html());
+                            $("#device-403-link").click(function () {
+                                window.location = "/iotserver/login";
+                            });
+                            showPopup();
+                        }else {
+                            $(modalPopupContent).html($('#download-device-modal-content-links').html());
+                            $("#download-device-url").val(data);
+                            $("#download-device-url").focus(function () {
+                                $(this).select();
+                            });
+                            showPopup();
+                        }
+                    }else{
+                        $(modalPopupContent).html($('#device-403-content').html());
+                        $("#device-403-link").click(function () {
+                            window.location.reload();
+                        });
+                        showPopup();
+                    }
+                },
+                function () {
+                    $(modalPopupContent).html($('#device-unexpected-error-content').html());
+                    $("a#device-unexpected-error-link").click(function () {
+                        hidePopup();
+                    });
+                }
+            );
+        });
+
+        $("a#download-device-cancel-link").click(function () {
+            hidePopup();
+        });
+
+    });
+}
