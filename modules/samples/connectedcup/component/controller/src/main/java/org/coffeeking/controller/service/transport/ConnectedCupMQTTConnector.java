6179298d9fa9b72eb73b88e1ab3e65f4615f63b7
Menaka Madushanka <menaka12350@gmail.com>
2016-01-13 11:11:55 +0530

    Connected cup device added

---
+++ modules/samples/connectedcup/component/controller/src/main/java/org/coffeeking/controller/service/transport/ConnectedCupMQTTConnector.java
@@ -0,0 +1,204 @@
+/*
+ * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.coffeeking.controller.service.transport;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.coffeeking.connectedcup.plugin.constants.ConnectedCupConstants;
+import org.coffeeking.controller.service.util.ConnectedCupServiceUtils;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.wso2.carbon.device.mgt.iot.config.server.DeviceManagementConfigurationManager;
+import org.wso2.carbon.device.mgt.iot.controlqueue.mqtt.MqttConfig;
+import org.wso2.carbon.device.mgt.iot.sensormgt.SensorDataManager;
+import org.wso2.carbon.device.mgt.iot.transport.TransportHandlerException;
+import org.wso2.carbon.device.mgt.iot.transport.mqtt.MQTTTransportHandler;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.util.Calendar;
+import java.util.UUID;
+
+@SuppressWarnings("no JAX-WS annotation")
+public class ConnectedCupMQTTConnector extends MQTTTransportHandler {
+    private static Log log = LogFactory.getLog(ConnectedCupMQTTConnector.class);
+
+    private static String serverName = DeviceManagementConfigurationManager.getInstance().
+            getDeviceManagementServerInfo().getName();
+
+    private static String subscribeTopic = "wso2" + File.separator + "+" + File.separator +
+                                           ConnectedCupConstants.DEVICE_TYPE + File.separator + "+" + File.separator
+                                           + "connected_publisher";
+
+    private static String iotServerSubscriber = UUID.randomUUID().toString().substring(0, 5);
+
+    private ConnectedCupMQTTConnector() {
+        super(iotServerSubscriber, ConnectedCupConstants.DEVICE_TYPE,
+              MqttConfig.getInstance().getMqttQueueEndpoint(), subscribeTopic);
+    }
+
+    @Override
+    public void connect() {
+        Runnable connector = new Runnable() {
+            public void run() {
+                while (!isConnected()) {
+                    try {
+                        connectToQueue();
+                        subscribeToQueue();
+                    } catch (TransportHandlerException e) {
+                        log.warn("Connection/Subscription to MQTT Broker at: " + mqttBrokerEndPoint + " failed");
+                        try {
+                            Thread.sleep(timeoutInterval);
+                        } catch (InterruptedException ex) {
+                            log.error("MQTT-Subscriber: Thread Sleep Interrupt Exception.", ex);
+                        }
+                    }
+                }
+            }
+        };
+
+        Thread connectorThread = new Thread(connector);
+        connectorThread.setDaemon(true);
+        connectorThread.start();
+    }
+
+
+
+    @Override
+    public void publishDeviceData(String... publishData) throws TransportHandlerException {
+        if (publishData.length != 4) {
+            String errorMsg = "Incorrect number of arguments received to SEND-MQTT Message. " +
+                    "Need to be [owner, deviceId, resource{BULB/TEMP}, state{ON/OFF or null}]";
+            log.error(errorMsg);
+            throw new TransportHandlerException(errorMsg);
+        }
+
+        String deviceOwner = publishData[0];
+        String deviceId = publishData[1];
+        String resource = publishData[2];
+        String state = publishData[3];
+
+        MqttMessage pushMessage = new MqttMessage();
+        String publishTopic =
+                serverName + File.separator + deviceOwner + File.separator +
+                        ConnectedCupConstants.DEVICE_TYPE + File.separator + deviceId;
+
+        try {
+
+            String actualMessage = resource + ":" + state;
+
+            pushMessage.setPayload(actualMessage.getBytes(StandardCharsets.UTF_8));
+            pushMessage.setQos(DEFAULT_MQTT_QUALITY_OF_SERVICE);
+            pushMessage.setRetained(false);
+
+            publishToQueue(publishTopic, pushMessage);
+
+        } catch (Exception e) {
+            String errorMsg = "Preparing Secure payload failed for device - [" + deviceId + "] of owner - " +
+                    "[" + deviceOwner + "].";
+            log.error(errorMsg);
+            throw new TransportHandlerException(errorMsg, e);
+        }
+    }
+
+
+    @Override
+    public void processIncomingMessage(MqttMessage mqttMessage, String... strings) throws TransportHandlerException {
+        String topic = strings[0];
+
+        String ownerAndId = topic.replace("wso2" + File.separator + "iot" + File.separator, "");
+        ownerAndId = ownerAndId.replace(File.separator + ConnectedCupConstants.DEVICE_TYPE + File.separator, ":");
+        ownerAndId = ownerAndId.replace(File.separator + "connectedcup_publisher", "");
+
+        String owner = ownerAndId.split(":")[0];
+        String deviceId = ownerAndId.split(":")[1];
+
+        String[] messageData = mqttMessage.toString().split(":");
+        Float value = Float.valueOf(messageData[1]);
+        switch (messageData[0]){
+            case "temperature":
+                SensorDataManager.getInstance().setSensorRecord(deviceId, ConnectedCupConstants.SENSOR_TEMPERATURE,
+                                                                String.valueOf(messageData[1]),
+                                                                Calendar.getInstance().getTimeInMillis());
+                ConnectedCupServiceUtils.publishToDAS(owner, deviceId, value);
+                break;
+            case "coffeelevel":
+                SensorDataManager.getInstance().setSensorRecord(deviceId, ConnectedCupConstants.SENSOR_TEMPERATURE,
+                                                                String.valueOf(messageData[1]),
+                                                                Calendar.getInstance().getTimeInMillis());
+                ConnectedCupServiceUtils.publishToDAS(owner, deviceId, value);
+        }
+
+        log.info("Received MQTT message for OWNER: " + owner + " DEVICE.ID: " + deviceId + " | Command: " +
+                 messageData[0] +" " + messageData[1] );
+
+
+    }
+
+
+    @Override
+    public void disconnect() {
+        Runnable stopConnection = new Runnable() {
+            public void run() {
+                while (isConnected()) {
+                    try {
+                        closeConnection();
+                    } catch (MqttException e) {
+                        if (log.isDebugEnabled()) {
+                            log.warn("Unable to 'STOP' MQTT connection at broker at: " + mqttBrokerEndPoint
+                                             + " for device-type - " + ConnectedCupConstants.DEVICE_TYPE, e);
+                        }
+
+                        try {
+                            Thread.sleep(timeoutInterval);
+                        } catch (InterruptedException e1) {
+                            log.error("MQTT-Terminator: Thread Sleep Interrupt Exception at device-type - " +
+                                                                        ConnectedCupConstants.DEVICE_TYPE, e1);
+                        }
+                    }
+                }
+            }
+        };
+
+        Thread terminatorThread = new Thread(stopConnection);
+        terminatorThread.setDaemon(true);
+        terminatorThread.start();
+    }
+
+    @Override
+    public void publishDeviceData() {
+        // nothing to do
+    }
+
+
+    @Override
+    public void publishDeviceData(MqttMessage publishData) throws TransportHandlerException {
+        // nothing to do
+    }
+
+    @Override
+    public void processIncomingMessage() {
+        // nothing to do
+    }
+
+    @Override
+    public void processIncomingMessage(MqttMessage message) throws TransportHandlerException {
+        // nothing to do
+    }
+}
---
+++ modules/samples/connectedcup/component/plugin/src/main/java/org/coffeeking/connectedcup/plugin/impl/dao/impl/ConnectedCupDAOImpl.java
@@ -0,0 +1,242 @@
+/*
+ * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.coffeeking.connectedcup.plugin.impl.dao.impl;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.coffeeking.connectedcup.plugin.constants.ConnectedCupConstants;
+import org.coffeeking.connectedcup.plugin.impl.dao.ConnectedCupDAO;
+import org.wso2.carbon.device.mgt.iot.util.iotdevice.dao.IotDeviceDAO;
+import org.wso2.carbon.device.mgt.iot.util.iotdevice.dao.IotDeviceManagementDAOException;
+import org.wso2.carbon.device.mgt.iot.util.iotdevice.dao.util.IotDeviceManagementDAOUtil;
+import org.wso2.carbon.device.mgt.iot.util.iotdevice.dto.IotDevice;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Implements IotDeviceDAO for virtual firealarm Devices.
+ */
+public class ConnectedCupDAOImpl implements IotDeviceDAO {
+	
+
+	    private static final Log log = LogFactory.getLog(ConnectedCupDAOImpl.class);
+
+	    @Override
+	    public IotDevice getIotDevice(String iotDeviceId)
+			    throws IotDeviceManagementDAOException {
+	        Connection conn = null;
+	        PreparedStatement stmt = null;
+	        IotDevice iotDevice = null;
+	        ResultSet resultSet = null;
+	        try {
+	            conn = ConnectedCupDAO.getConnection();
+	            String selectDBQuery =
+						"SELECT CONNECTED_CUP_DEVICE_ID, DEVICE_NAME, ACCESS_TOKEN, REFRESH_TOKEN" +
+						" FROM CONNECTED_CUP_DEVICE WHERE CONNECTED_CUP_DEVICE_ID = ?";
+	            stmt = conn.prepareStatement(selectDBQuery);
+	            stmt.setString(1, iotDeviceId);
+	            resultSet = stmt.executeQuery();
+
+	            if (resultSet.next()) {
+					iotDevice = new IotDevice();
+					iotDevice.setIotDeviceName(resultSet.getString(
+							ConnectedCupConstants.DEVICE_PLUGIN_DEVICE_NAME));
+					Map<String, String> propertyMap = new HashMap<String, String>();
+					propertyMap.put(ConnectedCupConstants.DEVICE_PLUGIN_PROPERTY_ACCESS_TOKEN, resultSet.getString("ACCESS_TOKEN"));
+					propertyMap.put(ConnectedCupConstants.DEVICE_PLUGIN_PROPERTY_REFRESH_TOKEN, resultSet.getString("REFRESH_TOKEN"));
+					iotDevice.setDeviceProperties(propertyMap);
+
+					if (log.isDebugEnabled()) {
+						log.debug("Connected Cup service " + iotDeviceId + " data has been fetched from " +
+						          "Connected Cup database.");
+					}
+				}
+	        } catch (SQLException e) {
+	            String msg = "Error occurred while fetching Connected Cup device : '" + iotDeviceId + "'";
+	            log.error(msg, e);
+	            throw new IotDeviceManagementDAOException(msg, e);
+	        } finally {
+	            IotDeviceManagementDAOUtil.cleanupResources(stmt, resultSet);
+	            ConnectedCupDAO.closeConnection();
+	        }
+
+	        return iotDevice;
+	    }
+
+	    @Override
+	    public boolean addIotDevice(IotDevice iotDevice)
+			    throws IotDeviceManagementDAOException {
+	        boolean status = false;
+	        Connection conn = null;
+	        PreparedStatement stmt = null;
+	        try {
+	            conn = ConnectedCupDAO.getConnection();
+		        String createDBQuery =
+				        "INSERT INTO CONNECTED_CUP_DEVICE(CONNECTED_CUP_DEVICE_ID, DEVICE_NAME, ACCESS_TOKEN, REFRESH_TOKEN) VALUES (?, ?, ?, ?)";
+
+		        stmt = conn.prepareStatement(createDBQuery);
+		        stmt.setString(1, iotDevice.getIotDeviceId());
+		        stmt.setString(2, iotDevice.getIotDeviceName());
+		        stmt.setString(3, iotDevice.getDeviceProperties().get(ConnectedCupConstants.DEVICE_PLUGIN_PROPERTY_ACCESS_TOKEN));
+		        stmt.setString(4, iotDevice.getDeviceProperties().get(ConnectedCupConstants.DEVICE_PLUGIN_PROPERTY_REFRESH_TOKEN));
+			
+				
+				int rows = stmt.executeUpdate();
+				if (rows > 0) {
+					status = true;
+					if (log.isDebugEnabled()) {
+						log.debug("Connected Cup device " + iotDevice.getIotDeviceId() + " data has been" +
+						          " added to the Connected Cup database.");
+					}
+				}
+	        } catch (SQLException e) {
+	            String msg = "Error occurred while adding the Connected Cup device '" +
+	                         iotDevice.getIotDeviceId() + "' to the Connected Cup db.";
+	            log.error(msg, e);
+	            throw new IotDeviceManagementDAOException(msg, e);
+	        } finally {
+	            IotDeviceManagementDAOUtil.cleanupResources(stmt, null);
+	        }
+	        return status;
+	    }
+
+	    @Override
+	    public boolean updateIotDevice(IotDevice iotDevice)
+			    throws IotDeviceManagementDAOException {
+	        boolean status = false;
+	        Connection conn = null;
+	        PreparedStatement stmt = null;
+	        try {
+	            conn = ConnectedCupDAO.getConnection();
+	            String updateDBQuery =
+						"UPDATE CONNECTED_CUP_DEVICE SET  DEVICE_NAME = ?, ACCESS_TOKEN=?, REFRESH_TOKEN=? WHERE CONNECTED_CUP_DEVICE_ID = ?";
+
+				stmt = conn.prepareStatement(updateDBQuery);
+
+				if (iotDevice.getDeviceProperties() == null) {
+					iotDevice.setDeviceProperties(new HashMap<String, String>());
+				}
+				stmt.setString(1, iotDevice.getIotDeviceName());
+				stmt.setString(2, iotDevice.getDeviceProperties().get(
+						ConnectedCupConstants.DEVICE_PLUGIN_PROPERTY_ACCESS_TOKEN));
+				stmt.setString(3, iotDevice.getDeviceProperties().get(
+						ConnectedCupConstants.DEVICE_PLUGIN_PROPERTY_ACCESS_TOKEN));
+				stmt.setString(4, iotDevice.getIotDeviceId());
+				int rows = stmt.executeUpdate();
+				if (rows > 0) {
+					status = true;
+					if (log.isDebugEnabled()) {
+						log.debug("Connected Cup device " + iotDevice.getIotDeviceId() + " data has been" +
+						          " modified.");
+					}
+				}
+	        } catch (SQLException e) {
+	            String msg = "Error occurred while modifying the Connected Cup device '" +
+	                         iotDevice.getIotDeviceId() + "' data.";
+	            log.error(msg, e);
+	            throw new IotDeviceManagementDAOException(msg, e);
+	        } finally {
+	            IotDeviceManagementDAOUtil.cleanupResources(stmt, null);
+	        }
+	        return status;
+	    }
+
+	    @Override
+	    public boolean deleteIotDevice(String iotDeviceId)
+			    throws IotDeviceManagementDAOException {
+	        boolean status = false;
+	        Connection conn = null;
+	        PreparedStatement stmt = null;
+	        try {
+	            conn = ConnectedCupDAO.getConnection();
+	            String deleteDBQuery =
+						"DELETE FROM CONNECTED_CUP_DEVICE WHERE CONNECTED_CUP_DEVICE_ID = ?";
+				stmt = conn.prepareStatement(deleteDBQuery);
+				stmt.setString(1, iotDeviceId);
+				int rows = stmt.executeUpdate();
+				if (rows > 0) {
+					status = true;
+					if (log.isDebugEnabled()) {
+						log.debug("Connected Cup device " + iotDeviceId + " data has deleted" +
+						          " from the Connected Cup database.");
+					}
+				}
+	        } catch (SQLException e) {
+	            String msg = "Error occurred while deleting Connected Cup device " + iotDeviceId;
+	            log.error(msg, e);
+	            throw new IotDeviceManagementDAOException(msg, e);
+	        } finally {
+	            IotDeviceManagementDAOUtil.cleanupResources(stmt, null);
+	        }
+	        return status;
+	    }
+
+	    @Override
+	    public List<IotDevice> getAllIotDevices()
+			    throws IotDeviceManagementDAOException {
+
+	        Connection conn = null;
+	        PreparedStatement stmt = null;
+	        ResultSet resultSet = null;
+	        IotDevice iotDevice;
+	        List<IotDevice> iotDevices = new ArrayList<>();
+
+	        try {
+	            conn = ConnectedCupDAO.getConnection();
+	            String selectDBQuery =
+						"SELECT CONNECTED_CUP_DEVICE_ID, DEVICE_NAME, ACCESS_TOKEN, REFRESH_TOKEN" +
+						"FROM CONNECTED_CUP_DEVICE";
+				stmt = conn.prepareStatement(selectDBQuery);
+				resultSet = stmt.executeQuery();
+				while (resultSet.next()) {
+					iotDevice = new IotDevice();
+					iotDevice.setIotDeviceId(resultSet.getString(ConnectedCupConstants.DEVICE_PLUGIN_DEVICE_ID));
+					iotDevice.setIotDeviceName(resultSet.getString(ConnectedCupConstants.DEVICE_PLUGIN_DEVICE_NAME));
+
+					Map<String, String> propertyMap = new HashMap<>();
+					propertyMap.put(ConnectedCupConstants.DEVICE_PLUGIN_PROPERTY_ACCESS_TOKEN,
+									resultSet.getString("ACCESS_TOKEN"));
+					propertyMap.put(ConnectedCupConstants.DEVICE_PLUGIN_PROPERTY_REFRESH_TOKEN,
+									resultSet.getString("REFRESH_TOKEN"));
+					iotDevice.setDeviceProperties(propertyMap);
+					iotDevices.add(iotDevice);
+				}
+	            if (log.isDebugEnabled()) {
+	                log.debug("All Connected Cup device details have fetched from Connected Cup database.");
+	            }
+	            return iotDevices;
+	        } catch (SQLException e) {
+	            String msg = "Error occurred while fetching all Connected Cup device data'";
+	            log.error(msg, e);
+	            throw new IotDeviceManagementDAOException(msg, e);
+	        } finally {
+	            IotDeviceManagementDAOUtil.cleanupResources(stmt, resultSet);
+	            ConnectedCupDAO.closeConnection();
+	        }
+	        
+	    }
+
+	}
