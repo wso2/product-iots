84dc857811364301a8ce90a77cdfa7313d5e436f
GPrathap <geesara@wso2.com>
2016-01-19 23:28:53 +0530

    facilitated to create a feature of given device type dynamically

--- modules/tools/cdmf-devicetype-archetype/src/main/resources/archetype-resources/feature/feature/src/main/resources/dbscripts/h2.sql
+++ modules/tools/cdmf-devicetype-archetype/src/main/resources/archetype-resources/feature/feature/src/main/resources/dbscripts/h2.sql
@@ -1,4 +1,11 @@
 
+-- -----------------------------------------------------
+--  Agent Database
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `${deviceType}_DEVICE` (
+  `${deviceType}_DEVICE_ID` VARCHAR(45) NOT NULL ,
+  `DEVICE_NAME` VARCHAR(100) NULL DEFAULT NULL,
+  PRIMARY KEY (`${deviceType}_DEVICE_ID`) );
 
 
 
