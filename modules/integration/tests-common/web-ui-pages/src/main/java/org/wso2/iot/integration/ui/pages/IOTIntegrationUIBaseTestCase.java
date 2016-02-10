d9799fa3c039a5fbc8fb2d995e8179789a315e3a
Menaka Madushanka <menaka12350@gmail.com>
2016-02-10 14:18:11 +0530

    License headers

--- modules/integration/tests-common/web-ui-pages/src/main/java/org/wso2/iot/integration/ui/pages/IOTIntegrationUIBaseTestCase.java
+++ modules/integration/tests-common/web-ui-pages/src/main/java/org/wso2/iot/integration/ui/pages/IOTIntegrationUIBaseTestCase.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.wso2.iot.integration.ui.pages;
 
 import org.apache.commons.logging.Log;
@@ -38,12 +55,11 @@ public class IOTIntegrationUIBaseTestCase {
             throws RemoteException, XPathExpressionException,
                    LoginAuthenticationExceptionException {
         AuthenticatorClient authenticationAdminClient = new AuthenticatorClient(context.getContextUrls().getBackEndUrl());
-        String sessionCookie = authenticationAdminClient.login(automationContext.getSuperTenant().
+        return authenticationAdminClient.login(automationContext.getSuperTenant().
                                                                        getTenantAdmin().getUserName(), automationContext.getSuperTenant().
                                                                        getTenantAdmin().getPassword(),
                                                                automationContext.getDefaultInstance().getHosts().get("default"));
 
-        return sessionCookie;
     }
 
     protected String getServiceURL() throws XPathExpressionException {
