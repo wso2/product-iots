9a0f0f2187c37015fd992ca45bfb466edb8eac96
Menaka Madushanka <menaka12350@gmail.com>
2016-02-25 11:21:18 +0530

    restart

---
+++ modules/integration/tests-iot-web-ui/src/test/java/org/wso2/carbon/iot/integration/web/ui/test/TestRestartServer.java
@@ -0,0 +1,7 @@
+package org.wso2.carbon.iot.integration.web.ui.test;
+
+/**
+ * Created by menaka on 2/23/16.
+ */
+public class TestRestartServer {
+}
---
+++ modules/integration/tests-iot-web-ui/src/test/java/org/wso2/carbon/iot/integration/web/ui/test/samples/SampleInstall.java
@@ -0,0 +1,7 @@
+package org.wso2.carbon.iot.integration.web.ui.test.samples;
+
+/**
+ * Created by menaka on 2/24/16.
+ */
+public class SampleInstall {
+}
