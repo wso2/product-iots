d5a56c462040801257672deb2e4363f85f576dd0
charitha <charitha.ws@gmail.com>
2016-06-27 09:55:37 +0530

    Add Smart Water Tank device type

---
+++ modules/samples/watertank/component/api/src/main/java/org/homeautomation/watertank/api/DeviceTypeService.java
@@ -0,0 +1,119 @@
+/*
+* Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+* WSO2 Inc. licenses this file to you under the Apache License,
+* Version 2.0 (the "License"); you may not use this file except
+* in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied. See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
+
+package org.homeautomation.watertank.api;
+
+import org.homeautomation.watertank.api.dto.DeviceJSON;
+
+import org.wso2.carbon.apimgt.annotations.api.API;
+import org.wso2.carbon.apimgt.annotations.api.Permission;
+import org.wso2.carbon.device.mgt.extensions.feature.mgt.annotations.DeviceType;
+import org.wso2.carbon.device.mgt.extensions.feature.mgt.annotations.Feature;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Path;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Produces;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.PUT;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+
+/**
+ * This is the controller API which is used to control agent side functionality
+ */
+@SuppressWarnings("NonJaxWsWebServices")
+@API(name = "watertank", version = "1.0.0", context = "/watertank", tags = "watertank")
+@DeviceType(value = "watertank")
+interface DeviceTypeService {
+
+    /**
+     * @param agentInfo device owner,id and sensor value
+     */
+    @Path("device/register")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Permission(scope = "watertank_user", permissions = {"/permission/admin/device-mgt/user/register"})
+    Response registerDevice(final DeviceJSON agentInfo);
+
+    /**
+     * @param deviceId unique identifier for given device type
+     * @param onLevel  level to turn on the relay
+     * @param offLevel level to turn off thr relay
+     */
+    @Path("device/{deviceId}/change-levels")
+    @POST
+    @Feature(code = "change-levels", name = "Change on/off water levels",
+            description = "Change on/off water levels")
+    @Permission(scope = "watertank_user", permissions = {"/permission/admin/device-mgt/change-levels"})
+    Response changeOnOffLevels(@PathParam("deviceId") String deviceId,
+                               @QueryParam("on") int onLevel,
+                               @QueryParam("off") int offLevel,
+                               @Context HttpServletResponse response);
+
+    /**
+     * Retrieve Sensor data for the given time period
+     * @param deviceId unique identifier for given device type instance
+     * @param sensorName name of the sensor
+     * @param from  starting time
+     * @param to    ending time
+     * @return  response with List<SensorRecord> object which includes sensor data which is requested
+     */
+    @Path("device/stats/{deviceId}/sensors/{sensorName}")
+    @GET
+    @Consumes("application/json")
+    @Produces("application/json")
+    Response getSensorStats(@PathParam("deviceId") String deviceId, @PathParam("sensorName") String sensorName,
+                                   @QueryParam("from") long from, @QueryParam("to") long to);
+
+    @Path("/device/{device_id}")
+    @DELETE
+    @Permission(scope = "watertank_user", permissions = {"/permission/admin/device-mgt/removeDevice"})
+    Response removeDevice(@PathParam("device_id") String deviceId);
+
+    @Path("/device/{device_id}")
+    @PUT
+    @Permission(scope = "watertank_user", permissions = {"/permission/admin/device-mgt/updateDevice"})
+    Response updateDevice(@PathParam("device_id") String deviceId, @QueryParam("name") String name);
+
+    @Path("/device/{device_id}")
+    @GET
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Permission(scope = "watertank_user", permissions = {"/permission/admin/device-mgt/updateDevice"})
+    Response getDevice(@PathParam("device_id") String deviceId);
+
+    @Path("/devices")
+    @GET
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Permission(scope = "watertank_user", permissions = {"/permission/admin/device-mgt/devices"})
+    Response getAllDevices();
+
+    @Path("/device/download")
+    @GET
+    @Produces("application/zip")
+    @Permission(scope = "watertank_user", permissions = {"/permission/admin/device-mgt/download"})
+    Response downloadSketch(@QueryParam("deviceName") String deviceName, @QueryParam("sketchType") String sketchType);
+}
---
+++ modules/samples/watertank/feature/feature/src/main/resources/agent/water-tank.lua
@@ -0,0 +1,76 @@
+DHT = require("dht_lib")
+
+dht_data = 2
+buzzer = 1
+gpio.mode(buzzer, gpio.OUTPUT)
+client_connected = false
+m = mqtt.Client("ESP8266-" .. node.chipid(), 120, "${DEVICE_TOKEN}", "")
+
+tmr.alarm(0, 10000, 1, function()
+    DHT.read(dht_data)
+
+    local t = DHT.getTemperature()
+    local h = DHT.getHumidity()
+
+    if t == nil then
+        print("Error reading from DHTxx")
+    else
+        if (client_connected) then
+            local payload = "{event:{metaData:{owner:\"${DEVICE_OWNER}\",deviceId:\"${DEVICE_ID}\"},payloadData:{temperature:" .. t .. ", humidity:" .. h .. "}}}"
+            m:publish("carbon.super/watertank/${DEVICE_ID}/data", payload, 0, 0, function(client)
+                print("Published> Temperature: " .. t .. "C  Humidity: " .. h .. "%")
+            end)
+        else
+            connectMQTTClient()
+        end
+    end
+end)
+
+function connectMQTTClient()
+    local ip = wifi.sta.getip()
+    if ip == nil then
+        print("Waiting for network")
+    else
+        print("Client IP: " .. ip)
+        print("Trying to connect MQTT client")
+        m:connect("${MQTT_EP}", $ { MQTT_PORT } , 0 , function (client)
+        client_connected = true
+        print("MQTT client connected")
+        subscribeToMQTTQueue()
+    end)
+end
+
+end
+
+function subscribeToMQTTQueue()
+m: subscribe("carbon.super/watertank/${DEVICE_ID}/command", 0, function(client, topic, message)
+print("Subscribed to MQTT Queue")
+end)
+m: on("message", function(client, topic, message)
+print("MQTT message received")
+print(message)
+buzz(message == "on")
+end)
+m: on("offline", function(client)
+print("Disconnected")
+client_connected = false
+end)
+end
+
+function buzz(status)
+local buzzerOn = true
+if(status) then
+tmr. alarm(1, 500, 1, function()
+if buzzerOn then
+buzzerOn = false
+gpio. write(buzzer, gpio. HIGH)
+else
+buzzerOn = true
+gpio. write(buzzer, gpio. LOW)
+end
+end)
+else
+tmr. stop(1)
+gpio. write(buzzer, gpio. LOW)
+end
+end
