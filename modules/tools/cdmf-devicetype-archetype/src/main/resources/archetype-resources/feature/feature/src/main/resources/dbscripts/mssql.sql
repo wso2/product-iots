56ec6b1e965252c796bda41217d27773a64b6f00
GPrathap <ggeesara@gmail.com>
2016-11-02 15:42:37 +0530

    removing cdmf devicetype archetype. This has been moved to https://github.com/wso2/carbon-device-mgt-maven-plugin

--- modules/tools/cdmf-devicetype-archetype/src/main/resources/archetype-resources/feature/feature/src/main/resources/dbscripts/mssql.sql
+++
@@ -1,9 +0,0 @@
-
--- -----------------------------------------------------
--- Table `${deviceType}_DEVICE`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS ${deviceType}_DEVICE (
-  ${deviceType}_DEVICE_ID VARCHAR(45) NOT NULL ,
-  DEVICE_NAME VARCHAR(100) NULL DEFAULT NULL,
-  PRIMARY KEY (${deviceType}_DEVICE_ID) );
-
