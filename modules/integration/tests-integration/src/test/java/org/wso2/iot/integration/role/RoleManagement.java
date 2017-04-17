828c59d0dd687d960ecb9fbb7cdfbea483e5897d
megala21 <ktumegala@yahoo.com>
2017-04-17 16:19:57 +0530

    Fixing test cases role management and user management

--- modules/integration/tests-integration/src/test/java/org/wso2/iot/integration/role/RoleManagement.java
+++ modules/integration/tests-integration/src/test/java/org/wso2/iot/integration/role/RoleManagement.java
@@ -19,44 +19,103 @@ package org.wso2.iot.integration.role;
 
 import junit.framework.Assert;
 import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.net.util.Base64;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Factory;
 import org.testng.annotations.Test;
 import org.wso2.carbon.automation.engine.context.TestUserMode;
+import org.wso2.carbon.automation.engine.context.beans.User;
 import org.wso2.iot.integration.common.*;
+
+import javax.xml.xpath.XPathExpressionException;
+import java.io.FileNotFoundException;
 
 /**
  * This class contains integration tests for role management backend services.
  */
 public class RoleManagement extends TestBase {
-
     private IOTHttpClient client;
+    private TestUserMode userMode;
+
+    @Factory(dataProvider = "userModeProvider")
+    public RoleManagement(TestUserMode userMode) {
+        this.userMode = userMode;
+    }
 
     @BeforeClass(alwaysRun = true, groups = { Constants.RoleManagement.ROLE_MANAGEMENT_GROUP})
     public void initTest() throws Exception {
-        super.init(TestUserMode.SUPER_TENANT_ADMIN);
-        String accessTokenString = "Bearer " + OAuthUtil.getOAuthToken(backendHTTPSURL, backendHTTPSURL);
+        super.init(userMode);
+        User currentUser = getAutomationContext().getContextTenant().getContextUser();
+        byte[] bytesEncoded = Base64
+                .encodeBase64((currentUser.getUserName() + ":" + currentUser.getPassword()).getBytes());
+        String encoded = new String(bytesEncoded);
+        String accessTokenString = "Bearer " + OAuthUtil
+                .getOAuthTokenPair(encoded, backendHTTPSURL, backendHTTPSURL, currentUser.getUserName(),
+                        currentUser.getPassword());
         this.client = new IOTHttpClient(backendHTTPSURL, Constants.APPLICATION_JSON, accessTokenString);
     }
 
     @Test(description = "Test add role.")
-    public void testAddRole() throws Exception {
-        IOTResponse response = client.post(Constants.RoleManagement.ADD_ROLE_ENDPOINT,
+    public void testAddRole() throws FileNotFoundException {
+        IOTResponse response = client.post(Constants.RoleManagement.ROLE_MANAGEMENT_END_POINT,
                                            PayloadGenerator.getJsonPayload(Constants.RoleManagement.ROLE_PAYLOAD_FILE_NAME,
                                                                            Constants.HTTP_METHOD_POST).toString());
         Assert.assertEquals(HttpStatus.SC_CREATED, response.getStatus());
     }
 
     @Test(description = "Test update permission role.", dependsOnMethods = {"testAddRole"})
-    public void testUpdateRolePermission() throws Exception {
-        IOTResponse response = client.put(Constants.RoleManagement.UPDATE_ROLE_PERMISSION_ENDPOINT,
-                                          PayloadGenerator.getJsonPayload(Constants.RoleManagement.UPDATE_ROLE_PERMISSION_PAYLOAD_FILE_NAME,
+    public void testUpdateRolePermission() throws FileNotFoundException {
+        IOTResponse response = client.put(Constants.RoleManagement.ROLE_MANAGEMENT_END_POINT + "/administration",
+                                          PayloadGenerator.getJsonPayload(Constants.RoleManagement.ROLE_PAYLOAD_FILE_NAME,
                                                                           Constants.HTTP_METHOD_PUT).toString());
         Assert.assertEquals(HttpStatus.SC_OK, response.getStatus());
     }
 
-    @Test(description = "Test remove user.", dependsOnMethods = {"testUpdateRolePermission"})
-    public void testRemoveRole() throws Exception {
-        IOTResponse response = client.delete(Constants.RoleManagement.REMOVE_ROLE_ENDPOINT);
+    @Test(description = "Test get roles.", dependsOnMethods = {"testUpdateRolePermission"})
+    public void testGetRoles() throws FileNotFoundException {
+        IOTResponse response = client.get(Constants.RoleManagement.ROLE_MANAGEMENT_END_POINT + "?offset=0&limit=2");
         Assert.assertEquals(HttpStatus.SC_OK, response.getStatus());
+        AssertUtil.jsonPayloadCompare(PayloadGenerator
+                .getJsonPayload(Constants.RoleManagement.ROLE_RESPONSE_PAYLOAD_FILE_NAME,
+                        Constants.UserManagement.GET_ROLES_METHOD).toString(), response.getBody(), true);
+    }
+
+    @Test(description = "Test getting roles that has particular prefix.", dependsOnMethods = {"testGetRoles"})
+    public void testGetFilteredRoles() throws FileNotFoundException {
+        IOTResponse response = client.get(Constants.RoleManagement.ROLE_MANAGEMENT_END_POINT +
+                "/filter/administ?offset=0&limit=2");
+        Assert.assertEquals(HttpStatus.SC_OK, response.getStatus());
+        AssertUtil.jsonPayloadCompare(PayloadGenerator
+                .getJsonPayload(Constants.RoleManagement.ROLE_RESPONSE_PAYLOAD_FILE_NAME,
+                        Constants.RoleManagement.GET_FILTERED_ROLED_METHOD).toString(), response.getBody(), true);
+    }
+
+    @Test(description = "Test getting permissions of a role.", dependsOnMethods = {"testGetFilteredRoles"})
+    public void testGetRolePermissions() throws FileNotFoundException {
+        IOTResponse response = client
+                .get(Constants.RoleManagement.ROLE_MANAGEMENT_END_POINT + "/administration/permissions");
+        Assert.assertEquals(HttpStatus.SC_OK, response.getStatus());
+    }
+
+    @Test(description = "Test getting role details.", dependsOnMethods = {"testGetRolePermissions"})
+    public void testGetRole() throws FileNotFoundException {
+        IOTResponse response = client.get(Constants.RoleManagement.ROLE_MANAGEMENT_END_POINT +
+                "/administration");
+        Assert.assertEquals(HttpStatus.SC_OK, response.getStatus());
+    }
+
+    @Test(description = "Test remove user.", dependsOnMethods = {"testGetRole"})
+    public void testRemoveRole() throws Exception {
+        IOTResponse response = client.delete(Constants.RoleManagement.ROLE_MANAGEMENT_END_POINT +"/administration");
+        Assert.assertEquals(HttpStatus.SC_OK, response.getStatus());
+    }
+
+    @DataProvider
+    private static Object[][] userModeProvider() {
+        return new TestUserMode[][]{
+                new TestUserMode[]{TestUserMode.SUPER_TENANT_ADMIN},
+                new TestUserMode[]{TestUserMode.TENANT_ADMIN}
+        };
     }
 }
