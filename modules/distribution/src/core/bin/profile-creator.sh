5ed7b60ae24c77b85f5aa03c669c9499d662090f
sinthuja <sinthuja@wso2.com>
2017-04-17 16:23:36 +0530

    Fixing SSO issue with keymanager profile.

--- modules/distribution/src/core/bin/profile-creator.sh
+++ modules/distribution/src/core/bin/profile-creator.sh
@@ -120,6 +120,7 @@ then
 	rm -rf ${DIR}/profile-creator.bat
 	cp -rf ${DIR}/../repository/resources/profiles/keymanager/*.sh ${DIR}/../bin/
 	cp -rf ${DIR}/../repository/resources/profiles/keymanager/carbon.xml ${DIR}/../conf/
+	cp -rf ${DIR}/../repository/resources/profiles/keymanager/identity/application-authentication.xml ${DIR}/../conf/identity/
 	rm -rf ${DIR}/../repository/deployment/server/jaggeryapps/*
 	rm -rf ${DIR}/../repository/deployment/server/synapse-configs/default/api/*
 	rm -rf ${DIR}/../repository/deployment/server/synapse-configs/default/sequences/_*.xml
@@ -127,6 +128,7 @@ then
 	cp ${DIR}/../repository/deployment/server/webapps/oauth2.war ${DIR}/../repository/deployment/server/tempwebapp/
 	cp ${DIR}/../repository/deployment/server/webapps/client-registration#v0.11.war ${DIR}/../repository/deployment/server/tempwebapp/
 	cp ${DIR}/../repository/deployment/server/webapps/dynamic-client-web.war ${DIR}/../repository/deployment/server/tempwebapp/
+	cp ${DIR}/../repository/deployment/server/webapps/authenticationendpoint.war ${DIR}/../repository/deployment/server/tempwebapp/
 	rm -rf ${DIR}/../repository/deployment/server/webapps/*
 	cp -rf ${DIR}/../repository/deployment/server/tempwebapp/* ${DIR}/../repository/deployment/server/webapps/
 	rm -rf ${DIR}/../repository/deployment/server/tempwebapp
