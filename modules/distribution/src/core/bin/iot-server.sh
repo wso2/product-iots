6ddc2da328b1974cc131ef47b904e244eb22d1e1
sinthuja <sinthuja@wso2.com>
2017-04-27 12:47:32 +0530

    Adding fixes related to ease distributed setup.

--- modules/distribution/src/core/bin/iot-server.sh
+++ modules/distribution/src/core/bin/iot-server.sh
@@ -326,6 +326,8 @@ do
     -Diot.gateway.host="localhost" \
     -Diot.gateway.https.port="8243" \
     -Diot.gateway.http.port="8280" \
+    -Diot.gateway.carbon.https.port="9448" \
+    -Diot.gateway.carbon.http.port="9768" \
     -Diot.apimpublisher.host="localhost" \
     -Diot.apimpublisher.https.port="9443" \
     -Diot.apimstore.host="localhost" \
