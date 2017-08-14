74509107489da5f67b4996c3bd9e5e4e05f0b242
Sameera Wickramasekara <sameeraw@wso2.com>
2017-08-14 17:15:51 +0530

    remove raspberrypicodebased devicetype

--- modules/distribution/src/core/samples/raspberrypicodebased/feature/feature/src/main/resources/dbscripts/oracle.sql
+++
@@ -1,9 +0,0 @@
-
--- -----------------------------------------------------
--- Table `raspberrypicodebased_DEVICE`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS raspberrypicodebased_DEVICE (
-  raspberrypicodebased_DEVICE_ID VARCHAR(45) NOT NULL ,
-  DEVICE_NAME VARCHAR(100) NULL DEFAULT NULL,
-  PRIMARY KEY (raspberrypicodebased_DEVICE_ID) );
-
