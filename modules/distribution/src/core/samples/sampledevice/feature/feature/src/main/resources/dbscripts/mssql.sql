273cb989d412bbb29297313b3ea7bae764e2ae10
Sameera Wickramasekara <sameeraw@wso2.com>
2017-08-14 17:17:57 +0530

    Add new device type sampledevice

---
+++ modules/distribution/src/core/samples/sampledevice/feature/feature/src/main/resources/dbscripts/mssql.sql
@@ -0,0 +1,9 @@
+
+-- -----------------------------------------------------
+-- Table `sampledevice_DEVICE`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS sampledevice_DEVICE (
+  sampledevice_DEVICE_ID VARCHAR(45) NOT NULL ,
+  DEVICE_NAME VARCHAR(100) NULL DEFAULT NULL,
+  PRIMARY KEY (sampledevice_DEVICE_ID) );
+
